d7f5528c1ed5fe347cbad09362f78aebb494418a
Florian Bütow <mail@florianbuetow.com>
2016-04-17 00:31:25 +0200

    p40

---
+++ 040_champernownes_constant.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# A Solution to "Champernowne's constant" – Project Euler Problem No. 40
+# by Florian Buetow
+#
+# Sourcecode: https://github.com/fbcom/project-euler
+# Problem statement: https://projecteuler.net/problem=40
+#
+
+
