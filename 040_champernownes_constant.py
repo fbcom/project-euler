a4f21c84402eac0ea8be7e8362f25091c5478eaa
Florian Bütow <mail@florianbuetow.com>
2016-04-17 00:31:52 +0200

    product times digit

--- 040_champernownes_constant.py
+++ 040_champernownes_constant.py
@@ -9,3 +9,17 @@
 #
 
 
+product = 1
+n = count = 0
+n_str = "0."
+while True:
+    n = n + 1
+    count = count + 1
+    n_str = n_str + str(n)
+    if count in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+        digit = int(n_str[count+1])
+        product *= digit
+    if count > 1000000:
+        break
+
+print "Solution", product
