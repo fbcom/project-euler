628836a2656aa2b70b3117ce4c0df7c61f5c8b86
Florian Buetow <mail@florianbuetow.com>
2016-04-30 22:36:14 +0200

    python optimizes exponentiation with present modulo

---
+++ 097_large_non_mersenne_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# A Solution to "Large non-mersenne prime" – Project Euler Problem No. 97
+# by Florian Buetow
+#
+# Sourcecode: https://github.com/fbcom/project-euler
+# Problem statement: https://projecteuler.net/problem=97
+
+
+print "Solution:", (28433 * 2**7830457 + 1) % 10**10
