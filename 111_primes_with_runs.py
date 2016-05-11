84fe41feedeaeb57f388c956b00fb37af3e972fb
Florian Buetow <mail@florianbuetow.com>
2016-05-11 21:01:26 +0200

    optimized

--- 111_primes_with_runs.py
+++ 111_primes_with_runs.py
@@ -8,6 +8,9 @@
 # Problem statement: https://projecteuler.net/problem=111
 
 
+primes = []
+
+
 def is_prime(n):
     if n < 2:
         return False
@@ -20,13 +23,26 @@ def is_prime(n):
             return False
     return True
 
-primes = []
+
+def is_prime_quick(n):
+    global primes
+    if n in primes:
+        return True
+    if n < primes[-1]:
+        return False
+    for d in primes:
+        if n % d == 0:
+            return False
+    return True
 
 
 def init_prime_array(limit):
+    print "Initializing prime array up to", limit
     global primes
     primes = [2]
     q = 3
+
+    percent_last = 0
     while q < limit:
         flag = True
         for p in primes:
@@ -37,102 +53,51 @@ def init_prime_array(limit):
                 break
         if flag:
             primes.append(q)
+            percent = 100 * (1.0*q/limit)
+            if int(percent) > percent_last:
+                print "%3.0f%s %d" % (percent, '%', q)
+                percent_last = int(percent)
         q = q + 2
+    print "Done initializing prime array up to", limit
 
 
-def M(n, d):
-    # Maximum number of repreated digit d of an n-digit prime
-    digit = str(d)
-    max_counter = 0
-    for p in primes:
-        if p <= 10**(n-1):
+def solve(digits):
+    global primes
+    limit = int(((10**digits)-1)**0.5)
+    init_prime_array(limit)
+    counter = [0]*10
+    max_counter = [0]*10
+    found_primes = [[]]*10
+    sums = [0]*10
+    n = 10**(digits-1)-1
+    max_n = 10**digits
+    while n < max_n:
+        n = n + 2
+        nstr = str(n)
+        if not is_prime_quick(n):
             continue
-        if p >= 10**(n):
-            break
-        q = str(p)
-        max_counter = max(max_counter, q.count(digit))
-    return max_counter
+        flag = False
+        for digit in range(10):
+            count = nstr.count(str(digit))
+            if count > max_counter[digit]:
+                max_counter[digit] = count
+                counter[digit] = 0
+                sums[digit] = 0
+                found_primes[digit] = []
 
+            if count == max_counter[digit]:
+                counter[digit] += 1
+                sums[digit] += n
+                found_primes[digit].append(n)
+                flag = True
 
-def N(n, d):
-    # Maximum number of repreated digit d of an n-digit prime
-    # return len(S(n,d))
-    max = M(n, d)
-    digit = str(d)
-    max_counter = 0
-    for p in primes:
-        if p <= 10**(n-1):
-            continue
-        if p >= 10**(n):
-            break
-        q = str(p)
-        if q.count(digit) == max:
-            max_counter += 1
-    return max_counter
-
-
-def S(n, d):
-    ret = []
-    max = M(n, d)
-    digit = str(d)
-    max_counter = 0
-    for p in primes:
-        if p <= 10**(n-1):
-            continue
-        if p >= 10**(n):
-            break
-        q = str(p)
-        if q.count(digit) == max:
-            ret.append(p)
-    return sum(ret)
-
-
-def solve(digits, d):
-    pass
+    return sum(sums)
 
 # Testcase
 digits = 4
-limit = 10**(digits+1)  # 4 digit primes
-init_prime_array(limit)
-
-assert M(digits, 0) == 2, "Testcase failed"
-assert M(digits, 1) == 3, "Testcase failed"
-assert M(digits, 2) == 3, "Testcase failed"
-assert M(digits, 3) == 3, "Testcase failed"
-assert M(digits, 4) == 3, "Testcase failed"
-assert M(digits, 5) == 3, "Testcase failed"
-assert M(digits, 6) == 3, "Testcase failed"
-assert M(digits, 7) == 3, "Testcase failed"
-assert M(digits, 8) == 3, "Testcase failed"
-assert M(digits, 9) == 3, "Testcase failed"
-
-assert N(digits, 0) == 13, "Testcase failed"
-assert N(digits, 1) == 9, "Testcase failed"
-assert N(digits, 2) == 1, "Testcase failed"
-assert N(digits, 3) == 12, "Testcase failed"
-assert N(digits, 4) == 2, "Testcase failed"
-assert N(digits, 5) == 1, "Testcase failed"
-assert N(digits, 6) == 1, "Testcase failed"
-assert N(digits, 7) == 9, "Testcase failed"
-assert N(digits, 8) == 1, "Testcase failed"
-assert N(digits, 9) == 7, "Testcase failed"
-
-assert S(digits, 0) == 67061, "Testcase failed"
-assert S(digits, 1) == 22275, "Testcase failed"
-assert S(digits, 2) == 2221, "Testcase failed"
-assert S(digits, 3) == 46214, "Testcase failed"
-assert S(digits, 4) == 8888, "Testcase failed"
-assert S(digits, 5) == 5557, "Testcase failed"
-assert S(digits, 6) == 6661, "Testcase failed"
-assert S(digits, 7) == 57863, "Testcase failed"
-assert S(digits, 8) == 8887, "Testcase failed"
-assert S(digits, 9) == 48073, "Testcase failed"
+assert 273700 == solve(digits), "Testcase failed"
 
 # Solve
 digits = 10
-limit = 10**(digits+1)
-init_prime_array(limit)
-for d in range(digits):
-    solution = solution + S(digits, d)
-
+solution = solve(digits)
 print "Solution:", solution
